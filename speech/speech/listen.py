b55fac7baf396fbbdf422b0fb2d7fd750fd1c17a
shayanhematpour-rgb <shayanhematpour@gmail.com>
2026-06-10 00:55:56 +0330

    Create listen.py

---
+++ speech/speech/listen.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+def listen():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+    try:
+        command = r.recognize_google(audio, language="en-US")
+        print("You said:", command)
+        return command
+    except:
+        return ""
+      
