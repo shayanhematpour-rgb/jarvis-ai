d2857f708c54b95dadd05acbaf9788d518ae45b7
shayanhematpour-rgb <shayanhematpour@gmail.com>
2026-06-10 00:57:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 print("Jarvis AI started...")
+from speech.speak import speak
+from speech.listen import listen
+
+speak("Jarvis is online")
+
+while True:
+    command = listen().lower()
+
+    if "hello" in command:
+        speak("Hello boss")
+
+    elif "your name" in command:
+        speak("I am Jarvis")
+
+    elif "stop" in command:
+        speak("Goodbye")
+        break
+
+    else:
+        speak("I did not understand")
+      
